6b0fd5b9cd6d32cfa538237ce76c31a6118ddc07
ecoregreso <ecoregreso@gmail.com>
2026-01-23 00:47:32 -0800

    fix: use explicit api path in shiftsApi

--- src/api/shiftsApi.js
+++ src/api/shiftsApi.js
@@ -1,4 +1,4 @@
-import api from "./api";
+import api from "./api.js";
 
 export async function fetchShiftSummary(date) {
   const res = await api.get("/api/v1/admin/shifts/summary", { params: { date } });
